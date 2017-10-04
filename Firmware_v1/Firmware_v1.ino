68a5189e9e729851b019b9e332b952cdb856399b
Bob_work <musgravehill@gmail.com>
2017-10-04 09:56:04 +0300

    fw

--- Firmware_v1/Firmware_v1.ino
+++ Firmware_v1/Firmware_v1.ino
@@ -1,3 +1,23 @@
+#include <stdlib.h>
+
+//=======================INIT IN-OUT======================================================
+
+#define STEPPER_UDO_DRIVER_notEN 12 //PB4
+#define STEPPER_UDO_DRIVER_STEP 10 //PB2
+#define STEPPER_UDO_DRIVER_DIR 11 //PB3
+
+#define BUZZER 5 //PD5
+#define BUTTON_udo_add 6 //PD6
+#define BUTTON_udo_pull 7 //PD7
+#define SENSOR_END_1 8 //PB0
+#define SENSOR_END_2 9 //PB1
+
+#define STEPPER_UDO_DIR_push true;
+#define STEPPER_UDO_DIR_pull false;
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
---
+++ Firmware_v1/STEPPER_UDO.ino
@@ -0,0 +1,35 @@
+void STEPPER_UDO_tick() {
+  PORTB |= _BV(PB2); //high
+  delayMicroseconds(8); //DRV8825 needs 1.9 us
+  PORTB &= ~_BV(PB2); //low
+}
+
+void STEPPER_UDO_dir(bool dir_forward) {
+  if (dir_forward) {
+    PORTB |= _BV(PB3); //high
+  } else {
+    PORTB &= ~_BV(PB3); //low
+  }
+}
+
+void STEPPER_UDO_en(bool is_en) {
+  if (is_en) {
+    PORTB &= ~_BV(PB4); //low
+  } else {
+    PORTB |= _BV(PB4); //high
+  }
+  delay(1000); //in ms
+}
+
+void STEPPER_UDO_init() {
+  pinMode(STEPPER_UDO_DRIVER_STEP,  OUTPUT);
+  digitalWrite(STEPPER_UDO_DRIVER_STEP, LOW); //no step
+
+  pinMode(STEPPER_UDO_DRIVER_DIR,  OUTPUT);
+  STEPPER_UDO_dir(STEPPER_UDO_DIR_push);
+
+  pinMode(STEPPER_UDO_DRIVER_notEN,  OUTPUT);
+  STEPPER_UDO_en(false);
+
+  TIMER_STEPPER_UDO_config;
+}
